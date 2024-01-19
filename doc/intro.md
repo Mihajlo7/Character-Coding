b4581e20a8d4c972028c090cfcbb8980f098b69c
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-01-19 08:40:31 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to character_coding
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/character_coding/core_test.clj
@@ -0,0 +1,7 @@
+(ns character-coding.core-test
+  (:require [clojure.test :refer :all]
+            [character-coding.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
