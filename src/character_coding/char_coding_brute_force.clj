5759b29733863db07d485c11c92aa9b0aad17903
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-01-19 11:59:15 +0100

    Added file which represent brute force algorithm for alphabet coding. Added function which returns all combinations for input letters

---
+++ src/character_coding/char_coding_brute_force.clj
@@ -0,0 +1,26 @@
+(ns character-coding.char-coding-brute-force
+  (:require [clojure.math.combinatorics :as comb]))
+
+; Add some input character and their frequences
+(def letters [['A' 5]
+              ['B' 1]
+              ['C' 6]
+              ['D' 3]])
+
+
+; If we use the standard method of storing characters, each character is represented as an 8-bit value,
+; and since there are a total of 15 letters, this text occupies 128 bits.
+; We will try to find an algorithm so that each character represents a binary value, thus reducing the memory size needed for storage
+
+(defn get-all-combinations [letters]
+  "Computing all combinations. This is exhaustive and computationally expensive!"
+  (let [only-letter (map first letters)]
+    (mapv (fn [permutation]
+            (mapv (fn [char value]
+                    [char value]) only-letter permutation)) (comb/permutations (range (count only-letter))))))
+
+
+
+
+
+
